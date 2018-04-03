851695be11d721ab58488d2d7ecd91fe809f5970
Cl3r1k <Cl3r1k2@gmail.com>
2018-04-03 18:47:29 +0600

    ✔ Added 'Fork on github' in the corner or github share link ✔ Added spinner before Angular main component loaded ✔ Changed flexbox to CSS Grids ✔ Added footer (preferably fixed bottom) ✔ Steaky-footer on CSS Grid Many minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-    title = 'app';
+export class AppComponent implements OnInit {
+    hiddenSocialIcons = true;
+    iconsHovered = false;
+
+    constructor() { }
+
+    ngOnInit() {
+        this.initApp();
+        const el = document.getElementById('loader-wrapper');
+        if (el) {
+            el.classList.add('loaded');
+        }
+    }
+
+    initApp() {
+        this.hiddenSocialIcons = false;
+    }
+
+    toggleSocialIconsState() {
+        this.hiddenSocialIcons = !this.hiddenSocialIcons;
+    }
 }
