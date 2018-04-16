45555d84ecef5c4313bb39434d5ed83f9d16fed1
Cl3r1k <Cl3r1k2@gmail.com>
2018-04-16 17:54:21 +0600

    Initial UI built

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
     hiddenSocialIcons = true;
     iconsHovered = false;
+    generation = 0;
 
     constructor() { }
 
