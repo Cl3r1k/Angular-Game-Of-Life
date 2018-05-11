4d4af7c3cdb20b1ed0e352d2996536fdbdc63f63
Cl3r1k <Cl3r1k2@gmail.com>
2018-05-11 16:48:35 +0600

    Completed UI prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,19 @@ export class AppComponent implements OnInit {
     iconsHovered = false;
     generation = 0;
 
+    lengthX = 30;
+    lengthY = 50;
+    speed = 5;
+
+    grid = [];
+    // grid = [
+    //     [ 0, 0, 1, 1, 1, 0, 0 ],
+    //     [ 0, 0, 0, 1, 0, 0, 0 ],
+    //     [ 0, 0, 1, 1, 0, 0, 0 ],
+    //     [ 0, 0, 0, 0, 0, 0, 0 ],
+    //     [ 0, 0, 0, 0, 0, 1, 0 ],
+    // ];
+
     constructor() { }
 
     ngOnInit() {
@@ -22,9 +35,38 @@ export class AppComponent implements OnInit {
 
     initApp() {
         this.hiddenSocialIcons = false;
+
+        this.resetGrid();
     }
 
     toggleSocialIconsState() {
         this.hiddenSocialIcons = !this.hiddenSocialIcons;
+    }
+
+    toggle(x: number, y: number) {
+        // console.log('x: %d, y: %d, content is: %d', x, y, this.grid[x][y]);
+        this.grid[x][y] = this.grid[x][y] === 0 ? 1 : 0;
+    }
+
+    resetGrid() {
+        this.grid = [];
+
+        for (let x = 0; x < this.lengthX; x++) {
+            const row: number[] = [];
+            for (let y = 0; y < this.lengthY; y++) {
+                row.push(Math.round(Math.random() - 0.2));    // Decrease random on 0.2 to get less 'alive' cells
+            }
+            this.grid.push(row);
+        }
+    }
+
+    setGridSize(lengthX: number, lengthY: number) {
+        this.lengthX = lengthX;
+        this.lengthY = lengthY;
+        this.resetGrid();
+    }
+
+    setSpeed(speed: number) {
+        this.speed = speed;
     }
 }
